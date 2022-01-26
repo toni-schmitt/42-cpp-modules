89e90f7da240efca86d3dd829bb03e35d90d12df
Toni Schmitt <tonischmitt@online.de>
2022-01-26 12:13:57 +0100

    add constructor

--- module-02/ex00/Fixed.cpp
+++ module-02/ex00/Fixed.cpp
@@ -6,11 +6,21 @@
 /*   By: toni <toni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 17:00:19 by toni              #+#    #+#             */
-/*   Updated: 2022/01/22 18:03:50 by toni             ###   ########.fr       */
+/*   Updated: 2022/01/26 12:11:59 by toni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	this->_fixedPointValue = 0;
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+	this->_fixedPointValue = fixed._fixedPointValue;
+}
 
 Fixed &Fixed::operator=(const Fixed &fixed)
 {
--- module-02/ex00/Fixed.hpp
+++ module-02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: toni <toni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 17:00:26 by toni              #+#    #+#             */
-/*   Updated: 2022/01/22 18:04:06 by toni             ###   ########.fr       */
+/*   Updated: 2022/01/26 12:12:43 by toni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ private:
 	static const int _noFractionalBits = 8;
 
 public:
-	Fixed() { _fixedPointValue = 0; }
-	Fixed(const Fixed &fixed) { _fixedPointValue = fixed.getRawBits(); }
-	~Fixed() = default;
+	Fixed();
+	Fixed(const Fixed &fixed);
+	~Fixed() {};
 	Fixed & operator = (const Fixed &fixed);
 
 	int getRawBits() const;
