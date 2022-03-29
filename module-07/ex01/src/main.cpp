66d2725508ec5b0e101e6eb04b779c9beb30b36a
Toni Schmitt <tonischmitt@online.de>
2022-03-29 20:31:18 +0200

    format main

--- module-07/ex01/src/main.cpp
+++ module-07/ex01/src/main.cpp
@@ -2,13 +2,13 @@
 
 void testIntArr()
 {
-	int arr[] = { 5, 123, 3634, 13423534, 1242352 };
+	int arr[] = {5, 123, 3634, 13423534, 1242352};
 	iter(arr, 5, &print);
 }
 
 void testFloatArr()
 {
-	float arr[] = { 12312.3923f, 29384.234f, 12312.9f, 123.923234f };
+	float arr[] = {12312.3923f, 29384.234f, 12312.9f, 123.923234f};
 	iter(arr, 4, &print);
 }
 
@@ -18,7 +18,7 @@ void testStringArr()
 	iter(arr, 6, &print);
 }
 
-int main( void )
+int main(void)
 {
 
 	testIntArr();
