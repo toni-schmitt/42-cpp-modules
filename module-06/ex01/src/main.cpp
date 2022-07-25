f6e54b3276acd8bdaf4ce4dd75b50407a5f572bd
tschmitt <tschmitt@student.42heilbronn.de>
2022-07-25 16:26:51 +0200

    add: some more in depth test output

--- module-06/ex01/src/main.cpp
+++ module-06/ex01/src/main.cpp
@@ -11,12 +11,12 @@ struct Data
 	void *data;
 };
 
-uintptr_t serialize(Data* ptr)
+uintptr_t serialize(Data *ptr)
 {
 	return (reinterpret_cast<uintptr_t>(ptr));
 }
 
-Data* deserialize(uintptr_t raw)
+Data *deserialize(uintptr_t raw)
 {
 	return (reinterpret_cast<Data *>(raw));
 }
@@ -26,6 +26,17 @@ int main(void)
 
 	Data data;
 
+	std::cout << "------------------------------------------" << std::endl;
+	std::cout << "Data Struct:" << std::endl
+			  << "struct Data" << std::endl
+			  << "{" << std::endl
+			  << "\tint i;" << std::endl
+			  << "\tfloat f;" << std::endl
+			  << "\tchar c;" << std::endl
+			  << "\tbool b;" << std::endl
+			  << "\tvoid *data" << std::endl
+			  << "};" << std::endl;
+
 	data.i = 199;
 	data.f = 99.87f;
 	data.c = 'x';
@@ -33,37 +44,42 @@ int main(void)
 	data.data = &data.i;
 
 	std::cout << "------------------------------------------" << std::endl;
-	std::cout << "Fill Data Struct:" << std::endl;
-	std::cout << "data.i = 199;" << std::endl;
-	std::cout << "data.f = 99.87f;" << std::endl;
-	std::cout << "data.c = 'x';" << std::endl;
-	std::cout << "data.b = true;" << std::endl;
-	std::cout << "data.data = &data.i;" << std::endl;
+	std::cout << "Fill Data Struct:" << std::endl
+			  << "data.i = 199;" << std::endl
+			  << "data.f = 99.87f;" << std::endl
+			  << "data.c = 'x';" << std::endl
+			  << "data.b = true;" << std::endl
+			  << "data.data = &data.i;" << std::endl;
 
 	std::cout << "------------------------------------------" << std::endl;
-	std::cout << "Data Struct Members:" << std::endl;
-	std::cout << "i\t\t" << data.i << std::endl;
-	std::cout << "f\t\t" << data.f << std::endl;
-	std::cout << "c\t\t" << data.c << std::endl;
-	std::cout << "b\t\t" << data.b << std::endl;
-	std::cout << "data\t\t" << data.data << std::endl;
-	std::cout << "*((int *)data)\t" << *((int *)data.data) << std::endl;
-	
+	std::cout << "Data Struct Members:" << std::endl
+			  << "i\t\t" << data.i << std::endl
+			  << "f\t\t" << data.f << std::endl
+			  << "c\t\t" << data.c << std::endl
+			  << "b\t\t" << data.b << std::endl
+			  << "data\t\t" << data.data << std::endl
+			  << "*((int *)data)\t" << *((int *)data.data) << std::endl;
+
 	std::cout << "------------------------------------------" << std::endl;
-	std::cout << "Serialize data to raw uintptr" << std::endl;
+	std::cout << "Serialize data to raw uintptr" << std::endl
+			  << std::endl
+			  << "uintptr_t raw = serialize(&data);" << std::endl;
 	uintptr_t raw = serialize(&data);
-	std::cout << "\nDeserialize raw uintptr to Data pointer" << std::endl;
+	std::cout << "------------------------------------------" << std::endl;
+	std::cout << "\nDeserialize raw uintptr to Data pointer" << std::endl
+			  << std::endl
+			  << "Data *deserialized = deserialize(raw);" << std::endl;
 	Data *deserialized = deserialize(raw);
 	std::cout << "------------------------------------------" << std::endl;
 
-	std::cout << "Data (deserialized) Members:" << std::endl;
-	std::cout << "i\t\t" << deserialized->i << std::endl;
-	std::cout << "f\t\t" << deserialized->f << std::endl;
-	std::cout << "c\t\t" << deserialized->c << std::endl;
-	std::cout << "b\t\t" << deserialized->b << std::endl;
-	std::cout << "data\t\t" << deserialized->data << std::endl;
-	std::cout << "*((int *)data)\t" << *((int *)deserialized->data) << std::endl;
-	std::cout << "------------------------------------------" << std::endl;
+	std::cout << "Data (deserialized) Members:" << std::endl
+			  << "i\t\t" << deserialized->i << std::endl
+			  << "f\t\t" << deserialized->f << std::endl
+			  << "c\t\t" << deserialized->c << std::endl
+			  << "b\t\t" << deserialized->b << std::endl
+			  << "data\t\t" << deserialized->data << std::endl
+			  << "*((int *)data)\t" << *((int *)deserialized->data) << std::endl
+			  << "------------------------------------------" << std::endl;
 
 	return 0;
 }
